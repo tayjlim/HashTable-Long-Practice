0a3e89d266510d86d6cea80048296d7ae2f6285a
Jack Roybal <excusemeimjack@gmail.com>
2023-01-26 15:13:57 -0800

    anagrams complete:

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,10 +1,37 @@
 function anagrams(str1, str2) {
-  // Your code here
+  if(str1.length !== str2.length) return false;
+  let obj1 = {};
+  let obj2 = {};
+  for(let i = 0; i < str1.length; i++){
+    let char = str1[i];
+    if(!obj1[char]) {
+      obj1[char] = 1;
+    } else {
+      obj1[char] += 1;
+    }
+  }
+  for(let i = 0; i < str2.length; i++) {
+    let char = str2[i];
+    if(!obj2[char]) {
+      obj2[char] = 1;
+    } else {
+      obj2[char] += 1;
+    }
+  }
+
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+  for(let key in obj1) {
+    if(obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 
 function commonElements(arr1, arr2) {
-  // Your code here
+
 }
 
 
