4b850c64e862f55c7d9c4bdffdfc4b6a57bc6509
Taylor <koo2true@gmail.com>
2023-01-26 16:16:01 -0800

    two sums op solution

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,3 +1,10 @@
+// function anagrams(str1, str2) {
+//   const set1 = new Set(str1.split(''));
+//   const set2 = new Set(str2.split(''));
+//   if(set1.size === set2.size && [...set1].every((char) => set2.has(char))) return true
+//   return false;
+// }
+
 function anagrams(str1, str2) {
   if(str1.length !== str2.length) return false;
   let obj1 = {};
@@ -18,7 +25,6 @@ function anagrams(str1, str2) {
       obj2[char] += 1;
     }
   }
-
   let keys1 = Object.keys(obj1);
   let keys2 = Object.keys(obj2);
   for(let key in obj1) {
@@ -33,7 +39,6 @@ function anagrams(str1, str2) {
 function commonElements(arr1, arr2) {
   let obj1 = {};
   let obj2 = {};
-
   let both = arr1.concat(arr2);
 
   for(let ele of arr1) {
@@ -54,19 +59,35 @@ function commonElements(arr1, arr2) {
 
 }
 
-
 function duplicate(arr) {
-  // Your code here
+  let obj = {};
+  for(let ele of arr){
+    if(!obj[ele]){
+      obj[ele] =1;
+    }else{
+      obj[ele] ++
+    }
+  }
+  for(let key in obj){
+    if(obj[key]>1){
+      return Number(key)
+    }
+  }
 }
 
-
-function twoSum(nums, target) {
-  // Your code here
+function twoSum(nums, target){
+  let hashTable = {};
+  for (let num of nums) {
+      if (hashTable[target - num]) {
+          return true;
+      }
+      hashTable[num] = true;
+  }
+  return false;
 }
-
 
 function wordPattern(pattern, strings) {
-  // Your code here
+
 }
 
 
