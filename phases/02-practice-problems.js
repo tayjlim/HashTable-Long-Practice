cb54cdebe2b2d9b5217136b174f13d62d8bea62c
Taylor <koo2true@gmail.com>
2023-01-26 16:22:39 -0800

    wow

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,39 +1,39 @@
-// function anagrams(str1, str2) {
-//   const set1 = new Set(str1.split(''));
-//   const set2 = new Set(str2.split(''));
-//   if(set1.size === set2.size && [...set1].every((char) => set2.has(char))) return true
-//   return false;
-// }
-
 function anagrams(str1, str2) {
-  if(str1.length !== str2.length) return false;
-  let obj1 = {};
-  let obj2 = {};
-  for(let i = 0; i < str1.length; i++){
-    let char = str1[i];
-    if(!obj1[char]) {
-      obj1[char] = 1;
-    } else {
-      obj1[char] += 1;
-    }
-  }
-  for(let i = 0; i < str2.length; i++) {
-    let char = str2[i];
-    if(!obj2[char]) {
-      obj2[char] = 1;
-    } else {
-      obj2[char] += 1;
-    }
-  }
-  let keys1 = Object.keys(obj1);
-  let keys2 = Object.keys(obj2);
-  for(let key in obj1) {
-    if(obj1[key] !== obj2[key]) {
-      return false;
-    }
-  }
-  return true;
+  const set1 = new Set(str1.split(''));
+  const set2 = new Set(str2.split(''));
+  if(set1.size === set2.size && [...set1].every((char) => set2.has(char))) return true
+  return false;
 }
+
+// function anagrams(str1, str2) {
+//   if(str1.length !== str2.length) return false;
+//   let obj1 = {};
+//   let obj2 = {};
+//   for(let i = 0; i < str1.length; i++){
+//     let char = str1[i];
+//     if(!obj1[char]) {
+//       obj1[char] = 1;
+//     } else {
+//       obj1[char] += 1;
+//     }
+//   }
+//   for(let i = 0; i < str2.length; i++) {
+//     let char = str2[i];
+//     if(!obj2[char]) {
+//       obj2[char] = 1;
+//     } else {
+//       obj2[char] += 1;
+//     }
+//   }
+//   let keys1 = Object.keys(obj1);
+//   let keys2 = Object.keys(obj2);
+//   for(let key in obj1) {
+//     if(obj1[key] !== obj2[key]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
 
 function commonElements(arr1, arr2) {
@@ -77,7 +77,7 @@ function duplicate(arr) {
 
 function twoSum(nums, target){
   let hashTable = {};
-  for (let num of nums) {
+  for (let num of nums){
       if (hashTable[target - num]) {
           return true;
       }
